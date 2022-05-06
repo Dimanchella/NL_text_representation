d6f05bcc3c83d1ea73a7ab41520fd77d6c2b4fa2
Nemo <vayag300300@yandex.ru>
2022-05-06 16:09:08 +0300

    remove hardcode connection string

--- LinguisticDatabase/Linguistic_DatabaseContext.cs
+++ LinguisticDatabase/Linguistic_DatabaseContext.cs
@@ -42,8 +42,7 @@ namespace LinguisticDatabase
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=10.243.57.89;Port=5432;Database=Linguistic_Database;Username=dima;Password=dima");
+                optionsBuilder.UseNpgsql(System.Configuration.ConfigurationManager.ConnectionStrings["LDB"].ConnectionString);
             }
         }
 
